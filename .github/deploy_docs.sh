edde87f05be95b102750e1ce27f077e7e5a0cfa6
LarsLemos <larslemos@gmail.com>
2023-05-03 18:22:59 +0200

    Include config update and deployment

--- .github/deploy_docs.sh
+++ .github/deploy_docs.sh
@@ -4,20 +4,22 @@
 
 if [ -d "ohri-docs" ]; then
     mv ohri-docs ohri-docs_backup_$(date +%Y%m%d_%H%M%S)
-    echo "OHRI Backup successful"
+    echo "--- OHRI Backup successful ---"
 else
-    echo "ohri-docs folder not found, backup not performed"
+    echo "--- ohri-docs folder not found, backup not performed ---"
 fi
 
 # Clone the repository with the reference branch named dev
-git clone --reference https://github.com/UCSF-IGHS/ohri-docs.git --branch dev https://github.com/UCSF-IGHS/ohri-docs.git
+git --branch dev https://github.com/UCSF-IGHS/ohri-docs.git
 
 # Navigate into the cloned directory
+echo "--- Navigate into the cloned directory ---"
 cd ohri-docs
 
 # Install dependencies and build the code
-yarn install
-yarn build
+echo "--- Installing dependencies and building the code ---"
+npx yarn install
+npx yarn build
 
 # Copy the built code to the specified path on the server using scp
 # scp -r ./build user@server:/usr/share/tomcat/microfrontends/ohri-docs
