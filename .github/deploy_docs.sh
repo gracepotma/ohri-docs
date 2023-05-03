eb07578e9336481aade9a9a876ee461c2c9f4031
LarsLemos <larslemos@gmail.com>
2023-05-03 09:09:11 +0200

    Improve the deploy script

--- .github/deploy_docs.sh
+++ .github/deploy_docs.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+# Rename the directory with suffix _backup_current_timestamp
+
+if [ -d "ohri-docs" ]; then
+    mv ohri-docs ohri-docs_backup_$(date +%Y%m%d_%H%M%S)
+    echo "OHRI Backup successful"
+else
+    echo "ohri-docs folder not found, backup not performed"
+fi
 
 # Clone the repository with the reference branch named dev
 git clone --reference https://github.com/UCSF-IGHS/ohri-docs.git --branch dev https://github.com/UCSF-IGHS/ohri-docs.git
@@ -11,8 +20,4 @@ yarn install
 yarn build
 
 # Copy the built code to the specified path on the server using scp
-scp -r ./build user@server:/usr/share/tomcat/microfrontends/ohri_docs
-
-# Navigate one level up and rename the directory with suffix _backup_current_timestamp
-cd ..
-mv ohri-docs ohri-docs_backup_$(date +%Y%m%d_%H%M%S)
+# scp -r ./build user@server:/usr/share/tomcat/microfrontends/ohri-docs
