cdfe287835e174c559c9c1f8543033848c131270
LarsLemos <larslemos@gmail.com>
2023-05-04 11:49:43 +0200

    Change the hidden folder

--- .github/deploy_docs.sh
+++ .github/deploy_docs.sh
@@ -3,6 +3,7 @@
 # Rename the directory with suffix _backup_current_timestamp
 
 if [ -d "ohri-docs" ]; then
+    cd ..
     mv ohri-docs ohri-docs_backup_$(date +%Y%m%d_%H%M%S)
     echo "--- OHRI Backup successful ---"
 else
@@ -26,6 +27,11 @@ ls -la -t
 
 # Copy the built code to the specified path on the server using scp
 # scp -r ./build user@server:/usr/share/tomcat/microfrontends/ohri-docs
-scp -P 3220 -r .next ohridocs@reports.globalhealthapp.net:/usr/share/tomcat/microfrontends/ohri_docs
+# scp -P 3220 -r .next ohridocs@reports.globalhealthapp.net:/usr/share/tomcat/microfrontends/ohri_docs
+
+echo "--- Copy Hiddne folder .next to build folder to allow transfer to another server ---"
+cp -R .next build
+
+scp -P 3220 -r build ohridocs@reports.globalhealthapp.net:/usr/share/tomcat/microfrontends/ohri_docs
 
 # scp -P 3220 -i /usr/local/ohridocs/.ssh/id_rsa -r .next ohridocs@reports.globalhealthapp.net:/usr/share/tomcat/microfrontends/ohri_docs
